c7c6e291bdf4455bbebe82c31311fa618257ca6f
Excel Erikefe <108849191+B3rry4r@users.noreply.github.com>
2025-07-12 07:57:40 +0100

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,25 @@
 import UIKit
 import Flutter
 import flutter_local_notifications
+import webview_flutter
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@main // Updated from @UIApplicationMain (Swift 5.3+)
+class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      
+    // WebView initialization (must come first)
+    if #available(iOS 11.0, *) {
+      WKWebView.apply()
     }
+    
+    // Notifications setup
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
