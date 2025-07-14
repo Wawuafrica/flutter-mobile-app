969b5da63cd8945385f0f8200c2065fe17e55a53
B3rry4r <berryterry914@gmail.com>
2025-07-14 20:24:44 +0100

    UPDATES

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
         applicationId = "com.wawuafrica.wawu"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        minSdk = flutter.minSdkVersion.toInt()
+        targetSdk = flutter.targetSdkVersion.toInt()
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
